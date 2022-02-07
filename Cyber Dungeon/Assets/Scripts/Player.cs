922a76e002a13c792123388e40e5a4e6cd8a6032
UNLUCKY <oss314y08@i.softbank.jp>
2022-02-07 16:26:23 +0900

    add update : Player Rotate

--- Cyber Dungeon/Assets/Scripts/Player.cs
+++ Cyber Dungeon/Assets/Scripts/Player.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+    GameObject HitPointObject;
+
+    GameObject obj;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        obj = (GameObject)Instantiate(HitPointObject,
+                Vector3.zero,
+                Quaternion.identity);
     }
 
     // Update is called once per frame
     void Update()
     {
         Movement();
+        TurnMousePosition();
     }
 
     void Movement()
@@ -45,6 +53,27 @@ public class Player : MonoBehaviour
         move.Normalize();
         move *= speed;
 
-        this.transform.Translate(move);
+        this.transform.position += move;
+    }
+
+    void TurnMousePosition()
+    {
+        // マウス位置座標取得
+        Vector3 mousePositon = Input.mousePosition;
+
+        // マウスのスクリーン座標からレイを飛ばす
+        Ray ray = Camera.main.ScreenPointToRay(mousePositon);
+        RaycastHit hit;
+
+        int mask = 1 << 9;
+
+        // レイと床の当たり判定
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity, mask))
+        {
+            obj.transform.position = hit.point;
+        }
+
+        // 床にレイがぶつかった方向を向く
+        this.transform.LookAt(hit.point);
     }
 }
